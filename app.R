83fcfb61369c67b39d83382b0c805601dcf715df
Gustavo <gmtofoli@gmail.com>
2020-03-24 01:56:14 -0300

    add recovered cases statistics

--- app.R
+++ app.R
@@ -4,9 +4,7 @@ library(plotly)
 library(shinycssloaders)
 
 header <- dashboardHeader(
-    title = tagList(
-        span(class = "logo-lg", "COVID-19"),
-        img(src = "" )),
+    title = "COVID-19",
     titleWidth = 187
 )
 
@@ -25,6 +23,7 @@ body <- dashboardBody(
             fluidRow(
                 infoBoxOutput("all_confirmed_info"),
                 infoBoxOutput("all_death_info"),
+                infoBoxOutput("all_recovered_info"),
                 infoBoxOutput("all_death_ratio_info")
             ),
             fluidRow(
@@ -63,19 +62,18 @@ variables <- reactiveValues(file_n_confirmed = NULL,
                             file_n_death = NULL,
                             n_confirmed_country = 0,
                             n_death_country = 0,
+                            n_recovered_country = 0,
                             all_confirmed = 0,
-                            all_death = 0)
+                            all_death = 0,
+                            all_recovered = 0)
 
 Download_Covid_Data <- function(url, output_file_name) {
     host <- "https://data.humdata.org"
     dest_file <- paste0("~/", output_file_name)
     file_path <- paste0(host, url)
     
-    # if (!file.exists(dest_file)) {
-    
     write.csv(data.frame(), output_file_name)
     download.file(file_path, dest_file)
-    # }
     
     data <- read.csv(file_path, header = TRUE, sep = ",")
     
@@ -87,19 +85,24 @@ server <- function(input, output) {
     output$select_country <- renderUI({
         url_n_confirmed <- "/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_19-covid-Confirmed.csv&filename=time_series_2019-ncov-Confirmed.csv"
         url_n_death <- "/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_19-covid-Deaths.csv&filename=time_series_2019-ncov-Deaths.csv"
+        url_n_recovered <- "/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_19-covid-Recovered.csv&filename=time_series_2019-ncov-Recovered.csv"
         
         n_confirmed_data <- Download_Covid_Data(url_n_confirmed, "n_confirmed.csv")
         n_death_data <- Download_Covid_Data(url_n_death, "n_death.csv")
+        n_recovered_data <- Download_Covid_Data(url_n_recovered, "n_recovered.csv")
         
         all_confirmed_data <- na.omit(n_confirmed_data[, ncol(n_confirmed_data)])
-        
         variables$all_confirmed <- sum(all_confirmed_data)
         
         all_death_data <- na.omit(n_death_data[ncol(n_death_data)])
         variables$all_death <- sum(all_death_data)
+        
+        all_recovered_data <- na.omit(n_recovered_data[ncol(n_recovered_data)])
+        variables$all_recovered <- sum(all_recovered_data)
        
         variables$file_n_confirmed <- n_confirmed_data
         variables$file_n_death <- n_death_data
+        variables$file_n_recovered <- n_recovered_data
         countries <- unique(n_confirmed_data$Country.Region)
         
         selectInput("select_country", label = h5("Select country"),
@@ -151,6 +154,17 @@ server <- function(input, output) {
         )
     })
     
+    output$all_recovered_info <- renderInfoBox({
+      all_recovered <- variables$all_recovered
+      infoBox(
+        "World recovered",
+        paste0(all_recovered),
+        icon = icon("check"),
+        color = "green", 
+        fill = TRUE
+      )
+    })
+    
     output$all_death_ratio_info <- renderInfoBox({
         all_confirmed <- variables$all_confirmed
         all_death <- variables$all_death
@@ -180,10 +194,12 @@ server <- function(input, output) {
     output$country_rank <- DT::renderDataTable({
       df_confirmed <- variables$file_n_confirmed
       df_death <- variables$file_n_death
+      df_recovered <- variables$file_n_recovered
       
       ctry = c()
       confirmed = c()
       death = c()
+      recovered = c()
       ratio = c()
       
       for (country in unique(df_confirmed$Country.Region)) {
@@ -193,26 +209,32 @@ server <- function(input, output) {
         subset_country_death <- subset(df_death, df_death$Country.Region == country)
         sum_death <- sum(subset_country_death[ncol(subset_country_death)])
         
+        subset_country_recovered <- subset(df_recovered, df_recovered$Country.Region == country)
+        sum_recovered <- sum(subset_country_recovered[ncol(subset_country_recovered)])
+        
         ratio_country <- round((sum_death/sum_confirmed)*100, 2)
         
         ctry = c(ctry, country)
         confirmed = c(confirmed, sum_confirmed)
         death = c(death, sum_death)
+        recovered = c(recovered, sum_recovered)
         ratio = c(ratio, ratio_country)
       }
       
-      df_country_rank <- data.frame(country = ctry, n_confirmed = confirmed, death = death, ratio = ratio)
+      df_country_rank <- data.frame(country = ctry, n_confirmed = confirmed, death = death, recovered = recovered, ratio = ratio)
       df_country_rank
     })
     
     output$confirmed_curve <- renderPlotly({
       n_confirmed_data <- variables$file_n_confirmed
       n_death_data <- variables$file_n_death
+      n_recovered_data <- variables$file_n_recovered
       
       country <- input$select_country
       
       country_c <- subset(n_confirmed_data, n_confirmed_data$Country.Region == country)
       country_d <- subset(n_death_data, n_death_data$Country.Region == country)
+      country_r <- subset(n_recovered_data, n_recovered_data$Country.Region == country)
       
       days <- colnames(n_confirmed_data)[5:ncol(country_c)]
       
@@ -234,8 +256,8 @@ server <- function(input, output) {
       }
       
       counts_c <- country_c[, 5: ncol(country_c)]
-      
       counts_d <- country_d[, 5: ncol(country_d)]
+      counts_r <- country_r[, 5: ncol(country_r)]
       
       n_c <- c()
       for (i in 1:length(counts_c)) {
@@ -247,12 +269,17 @@ server <- function(input, output) {
         n_d <- c(n_d, sum(counts_d[, i]))
       }
       
-      df_timeseries <- data.frame(day=days_new, n_c=n_c, n_d=n_d)
-      print(df_timeseries)
+      n_r <- c()
+      for (i in 1:length(counts_r)) {
+        n_r <- c(n_r, sum(counts_r[, i]))
+      }
+      
+      df_timeseries <- data.frame(day=days_new, n_c=n_c, n_d=n_d, n_r=n_r)
       
       plot <- plot_ly(data = df_timeseries, x = ~day, y = ~n_c, name = 'Confirmed', type = 'scatter', mode = 'lines')
       # plot_ly(data = df_timeseries, x = ~day, y = ~n_c, name = 'Confirmed', type = 'scatter', mode = 'lines') %>%
       plot <- plot %>% add_trace(y = ~n_d, name = 'Death', line = list(color = 'red'))
+      plot <- plot %>% add_trace(y = ~ n_r, name = 'Recovered', line = list(color = 'green'))
       plot<- plot %>% layout(yaxis = list(title="Occurrences"))
       plot
     })
